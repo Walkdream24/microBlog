dfeb0a464794b10b72ddff1543800a2f08e3dd8f
Walkdream24 <42060447+Walkdream24@users.noreply.github.com>
2019-04-02 19:58:13 +0900

    ページネーション追加

--- microblog/microblog/blog/views.py
+++ microblog/microblog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 from django.views.generic import ListView
 from django.views.generic import DetailView
@@ -14,28 +15,56 @@ from .forms import BlogForm
 
 class BlogListView(ListView):
     model = Blog
+    paginate_by = 5
+    context_object_name = "blogs"
 
 
 class BlogDetailView(DetailView):
     model = Blog
+    context_object_name = "blog"
 
 
 class BlogCreateView(CreateView):
     model = Blog
     form_class = BlogForm
     success_url = reverse_lazy("index")
+    template_name = "blog/blog_create_form.html"
+
+    def form_valid(self, form):
+        messages.success(self.request, "保存しました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "保存に失敗しました")
+        return super().form_invalid(form)
+
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     form_class = BlogForm
+    template_name = "blog/blog_update_form.html"
 
     def get_success_url(self):
         blog_pk = self.kwargs['pk']
         url = reverse_lazy("detail", kwargs={"pk": blog_pk})
         return url
 
+    def form_valid(self, form):
+        messages.success(self.request, "更新されました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "更新に失敗しました")
+        return super().form_invalid(form)
+
 
 class BlogDeleteView(DeleteView):
     model = Blog
     success_url = reverse_lazy("index")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "削除しました")
+        return super().delete(request, *args, **kwargs)
+
 
--- microblog/microblog/microblog/urls.py
+++ microblog/microblog/microblog/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('<int:pk>', BlogDetailView.as_view(), name='detail'),
     path('<int:pk>/update', BlogUpdateView.as_view(), name="update"),
     path('<int:pk>/delete', BlogDeleteView.as_view(), name="delete"),
-
     path('create', BlogCreateView.as_view(), name="create"),
     path('admin/', admin.site.urls),
 
